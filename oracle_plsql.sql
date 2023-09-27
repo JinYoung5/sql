1562c4c72c738a3af733baddacdfb380426d1901
JinYoung5 <gohaho5@naver.com>
2023-09-27 15:38:16 +0900

    9.27 새로운파일 PL/SQL

---
+++ oracle_plsql.sql
@@ -0,0 +1,9 @@
+PL/SQL
+Procedural language extension to Structured Query Language(SQL)의 약자
+SQL을 확장한 순차적 처리 언어
+
+PL/SQL의 기본 구조
+익명 블럭
+1)선언부(declarative part) : 사용할 변수나 상수를 선언(선언부에만 변수와 상수 선언 가능)
+2)실행부(executable part) : 실제 처리할 로직을 담당하는 부분
+3)예외처리부(exception-building part) : 실행부에서 로직을 처리하던 중 발생할 수 있는 각종 오류들에 대해 처리
